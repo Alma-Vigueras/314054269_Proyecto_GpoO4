8998586981a56fca5bed35e3a1c9f9916d5fdc56
Alma-Vigueras <alma.ramirez@ingenieria.unam.edu>
2022-05-25 23:30:11 -0500

    Avance

--- ProyectoFinal/ProyectoFinal/Main.cpp
+++ ProyectoFinal/ProyectoFinal/Main.cpp
@@ -59,7 +59,7 @@ int main()
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
     // Create a GLFWwindow object that we can use for GLFW's functions
-    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "314054269 Proyecto Final Lab 2022-2 Gpo04", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "314054269 Proyecto Final 2022-2 Gpo04", nullptr, nullptr);
 
     if (nullptr == window)
     {
@@ -103,6 +103,7 @@ int main()
     Model Puerta((char*)"Models/Door/Puerta2.obj"); 
     Model Wind((char*)"Models/Ventanas/Window1.obj");
     Model Wind2((char*)"Models/Ventanas/Window2.obj");
+    Model Cristal((char*)"Models/Ventanas/Glass.obj");
     Model Balcon((char*)"Models/Door/PuertaBalcon.obj");
     Model Grass((char*)"Models/Grass/Pasto.obj");
     Model Jardinera((char*)"Models/Jardinera/Planter.obj");
@@ -113,6 +114,10 @@ int main()
     Model Clock((char*)"Models/Clock/WallClock.obj");
     Model Projector((char*)"Models/Projector/Projector.obj");
     Model Woofer((char*)"Models/Woofer/bocina.obj");
+
+    Model Mesa((char*)"Models/Table/WoodTable.obj");
+    Model Silla((char*)"Models/Chair/Silla.obj");
+
     glm::mat4 projection = glm::perspective(camera.GetZoom(), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
 
 
@@ -155,6 +160,7 @@ int main()
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
         Puerta.Draw(shader);
 
+        //Puerta de balcon
         model = glm::mat4(1);
         model = glm::translate(model, glm::vec3(-0.95f, 4.55f, 5.15f));
         model = glm::rotate(model, glm::radians(rot), glm::vec3(0.0f, 1.0f, 0.0));
@@ -195,6 +201,7 @@ int main()
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
         Wind2.Draw(shader);
 
+
         //Jardinera
         model = glm::mat4(1);
         model = glm::translate(model, glm::vec3(-2.4f, -0.2f, 4.5f));
@@ -219,6 +226,7 @@ int main()
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
         Grass.Draw(shader);
 
+        //Proyector
         model = glm::mat4(1);
         model = glm::translate(model, glm::vec3(2.95f, 0.78f, 1.5f));
         model = glm::rotate(model, glm::radians(rot2), glm::vec3(0.0f, 1.0f, 0.0));
@@ -227,6 +235,7 @@ int main()
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
         Projector.Draw(shader);
 
+        //Reloj
         model = glm::mat4(1);
         model = glm::translate(model, glm::vec3(3.5f, 1.8f, 1.7f));
         model = glm::rotate(model, glm::radians(rot), glm::vec3(0.0f, 1.0f, 0.0));
@@ -235,6 +244,7 @@ int main()
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
         Clock.Draw(shader);
 
+        //Maceta anturio
         model = glm::mat4(1);
         model = glm::translate(model, glm::vec3(3.1f, 0.65f, 0.5f));
         //model = glm::rotate(model,glm::vec3(0.0f, 1.0f, 0.0));
@@ -243,6 +253,7 @@ int main()
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
         Ant.Draw(shader);
 
+        //Mesa
         model = glm::mat4(1);
         model = glm::translate(model, glm::vec3(2.9f, -0.3f, 0.8f));
         model = glm::rotate(model, glm::radians(rot), glm::vec3(0.0f, 1.0f, 0.0));
@@ -251,6 +262,7 @@ int main()
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
         table.Draw(shader);
 
+        //Sillon 1
         model = glm::mat4(1);
         model = glm::translate(model, glm::vec3(1.7f, -0.1f, 0.5f));
         //model = glm::rotate(model, glm::radians(rot), glm::vec3(0.0f, 1.0f, 0.0));
@@ -259,6 +271,7 @@ int main()
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
         sillon.Draw(shader);
 
+        //Sillon 2
         model = glm::mat4(1);
         model = glm::translate(model, glm::vec3(1.6f, -0.1f, 2.4f));
         model = glm::rotate(model, glm::radians(rot2), glm::vec3(0.0f, 1.0f, 0.0));
@@ -267,6 +280,7 @@ int main()
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
         sillon.Draw(shader);
 
+        //Banco
         model = glm::mat4(1);
         model = glm::translate(model, glm::vec3(3.1f, 0.5f, 0.5f));
         //model = glm::rotate(model,glm::vec3(0.0f, 1.0f, 0.0));
@@ -275,6 +289,7 @@ int main()
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
         banco.Draw(shader);
 
+        //Bocina
         model = glm::mat4(1);
         model = glm::translate(model, glm::vec3(3.1f, 0.49f, 2.5f));
         model = glm::rotate(model, glm::radians(rot), glm::vec3(0.0f, 1.0f, 0.0));
@@ -282,6 +297,26 @@ int main()
         model = glm::scale(model, glm::vec3(0.2f, 0.2f, 0.2f));
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
         Woofer.Draw(shader);
+
+        //Segunda Habitación
+
+        //Mesa
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(-1.0f, 0.2f, -3.0f));
+        model = glm::rotate(model, glm::radians(rot2), glm::vec3(0.0f, 1.0f, 0.0));
+        //model = glm::translate(model, glm::vec3(1.7f, 0.5f, -2.9f));
+        model = glm::scale(model, glm::vec3(3.5f, 3.5f, 3.5f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        Mesa.Draw(shader);
+
+        //silla 
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(-0.7f, 0.2f, -2.0f));
+        model = glm::rotate(model, glm::radians(rot), glm::vec3(0.0f, 1.0f, 0.0));
+        //model = glm::translate(model, glm::vec3(1.7f, 0.5f, -2.9f));
+        model = glm::scale(model, glm::vec3(1.4f, 1.4f, 1.4f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        Silla.Draw(shader);
 
         // Swap the buffers
         glfwSwapBuffers(window);
